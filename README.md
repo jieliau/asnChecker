e8ccad4d9df0b5abf4c132a8f0a4c0550cca59b8
Jie Liau <wjliau@tw.ibm.com>
2020-03-11 13:28:02 +0800

    Added some error handling, like no AS in peeringdb and Potaroo, AS number provided is not integer, etc

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# asnChecker
+# Autonoumous System Information Checker
+
+asnChecker is a tool to get information of the autonoumous system which you're interested. Such like Adj ASes, PoC, Prefixes, Public IXes and Private facs, etc. This tool will crawl the public information on Internet and return all of them using JSON format.
+
+# Description
+
+Running this tool will listen on 127.0.0.1 port 8080 and the only thing is to provide AS number to asn parameter to asnchecker. 
+>Example: http://127.0.0.1:8080/asnchecker?asn=15169
+
+# Installation
+
+```sh
+$ pip3 install -r requirements.txt
+$ python3 asnChecker.py
+$ * Serving Flask app "asnChecker" (lazy loading)
+$ * Environment: production
+$   WARNING: This is a development server. Do not use it in a production deployment.
+$   Use a production WSGI server instead.
+$ * Debug mode: on
+$ * Running on http://127.0.0.1:8080/ (Press CTRL+C to quit)
+$ * Restarting with stat
+$ * Debugger is active!
+$ * Debugger PIN: 328-083-063
+```
+
+# Docker
+
+It's on the way go
--- asnChecker.py
+++ asnChecker.py
@@ -10,19 +10,28 @@ def asNumberChecker(as_number):
     aspfx = [i.text for i in soup.select('pre a')]
     ases = list()
     prefixes = list()
-    for i in range(len(aspfx)):
-        if 'AS' in aspfx[i]:
-            ases.append(aspfx[i])
-    ases.remove("AS" + as_number)
-    prefixes = [i.text.strip(" ") for i in soup.select('.black')]
+    if aspfx == []:
+        ases = []
+        prefixes = []
+    else:    
+        for i in range(len(aspfx)):
+            if 'AS' in aspfx[i]:
+                ases.append(aspfx[i])
+        ases.remove("AS" + as_number)
+        prefixes = [i.text.strip(" ") for i in soup.select('.black')]
 
     """ Get rich information from peeringdb.com """
     #r = requests.get("https://peeringdb.com/api/net")
     #netjson = r.json()
+    isNet = False
+    pocs = list()
+    ixes = list()
+    facs = list()
     with open("./net.json") as netjson_file:
         netdata = json.load(netjson_file)
         for i in range(len(netdata['data'])):
             if netdata['data'][i]['asn'] == int(as_number):
+                isNet = True
                 name = netdata['data'][i]['name']
                 website = netdata['data'][i]['website']
                 supportIPv6 = netdata['data'][i]['info_ipv6']
@@ -30,40 +39,46 @@ def asNumberChecker(as_number):
                 net_id = netdata['data'][i]['id']
                 org_id = netdata['data'][i]['org_id']
                 break
+        if isNet == False:
+            name = 'N/A'
+            website = 'N/A'
+            supportIPv6 = 'N/A'
+            supportMulticast = 'N/A'
+            pocs = []
+            ixes = []
+            facs = []
+            country = 'N/A'
+        else:
+            r = requests.get("https://peeringdb.com/api/net/" + str(net_id))
+            netjson = r.json()
+            for i in range(len(netjson['data'][0]['poc_set'])):
+                poc = {
+                    "Name": netjson['data'][0]['poc_set'][i]['name'],
+                    "Role": netjson['data'][0]['poc_set'][i]['role'],
+                    "E-Mail": netjson['data'][0]['poc_set'][i]['email']
+                }
+                pocs.append(poc)
 
-    pocs = list()
-    ixes = list()
-    facs = list()
-    r = requests.get("https://peeringdb.com/api/net/" + str(net_id))
-    netjson = r.json()
-    for i in range(len(netjson['data'][0]['poc_set'])):
-        poc = {
-            "Name": netjson['data'][0]['poc_set'][i]['name'],
-            "Role": netjson['data'][0]['poc_set'][i]['role'],
-            "E-Mail": netjson['data'][0]['poc_set'][i]['email']
-        }
-        pocs.append(poc)
+            for i in range(len(netjson['data'][0]['netixlan_set'])):
+                ix = {
+                    "Name": netjson['data'][0]['netixlan_set'][i]['name'],
+                    "AS Number": netjson['data'][0]['netixlan_set'][i]['asn'],
+                    "IPv4": netjson['data'][0]['netixlan_set'][i]['ipaddr4'],
+                    "IPv6": netjson['data'][0]['netixlan_set'][i]['ipaddr6']
+                }
+                ixes.append(ix)
 
-    for i in range(len(netjson['data'][0]['netixlan_set'])):
-        ix = {
-            "Name": netjson['data'][0]['netixlan_set'][i]['name'],
-            "AS Number": netjson['data'][0]['netixlan_set'][i]['asn'],
-            "IPv4": netjson['data'][0]['netixlan_set'][i]['ipaddr4'],
-            "IPv6": netjson['data'][0]['netixlan_set'][i]['ipaddr6']
-        }
-        ixes.append(ix)
+            for i in range(len(netjson['data'][0]['netfac_set'])):
+                fac = {
+                    "Name": netjson['data'][0]['netfac_set'][i]['name'],
+                    "Country": netjson['data'][0]['netfac_set'][i]['country'],
+                    "City": netjson['data'][0]['netfac_set'][i]['city']
+                }
+                facs.append(fac)
 
-    for i in range(len(netjson['data'][0]['netfac_set'])):
-        fac = {
-            "Name": netjson['data'][0]['netfac_set'][i]['name'],
-            "Country": netjson['data'][0]['netfac_set'][i]['country'],
-            "City": netjson['data'][0]['netfac_set'][i]['city']
-        }
-        facs.append(fac)
-
-    r = requests.get("https://peeringdb.com/api/org/" + str(org_id))
-    orgjson = r.json()
-    country = orgjson['data'][0]['country']
+            r = requests.get("https://peeringdb.com/api/org/" + str(org_id))
+            orgjson = r.json()
+            country = orgjson['data'][0]['country']
 
     return name, website, country, supportIPv6, supportMulticast, pocs, ixes, facs, ases, prefixes
 
@@ -73,25 +88,32 @@ def main():
     
     @app.route("/", methods=['GET'])
     def root():
-        return "<h1>Open browser and connect to http://127.0.0.1:8080/asnchecker?asn=[your as number]</h1>"
+        return "<h1>Open browser and connect to http://127.0.0.1:8080/asnchecker?asn=[your AS number]</h1>"
     
     @app.route("/asnchecker", methods=['GET'])
     def asn():
-        asn = request.args.get('asn')
-        name, website, country, supportIPv6, supportMulticast, pocs, ixes, facs, ases, prefixes = asNumberChecker(asn)
-        results = {
-            "Name": name,
-            "Website": website,
-            "Country": country,
-            "Support IPv6": supportIPv6,
-            "Support Multicast": supportMulticast,
-            "Point Of Contacts": pocs,
-            "Public Peering Exchanges Points": ixes,
-            "Private Peering Facilities": facs,
-            "Adjancy ASes": ases,
-            "Prefixes": prefixes
-        }
-        return jsonify(results)
+        if 'asn' in request.args:
+            asn = request.args.get('asn')
+            try:
+                int(asn)
+                name, website, country, supportIPv6, supportMulticast, pocs, ixes, facs, ases, prefixes = asNumberChecker(asn)
+                results = {
+                    "Name": name,
+                    "Website": website,
+                    "Country": country,
+                    "Support IPv6": supportIPv6,
+                    "Support Multicast": supportMulticast,
+                    "Point Of Contacts": pocs,
+                    "Public Peering Exchanges Points": ixes,
+                    "Private Peering Facilities": facs,
+                    "Adjancy ASes": ases,
+                    "Prefixes": prefixes
+                }
+                return jsonify(results)
+            except ValueError:
+                return "<h1>You use the wrong AS number format. Integer</h1>"
+        else:
+            return "<h1>You have to provide AS number. http://127.0.0.1:8080/asnchecker?asn=[your AS number]</h1>"
 
     app.run(host="127.0.0.1", port="8080")
 
